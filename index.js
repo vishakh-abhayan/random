202db04554cbfcc4d9bc4d22e665df4804a8af88
Vishakh Abhayan <vishakhabhayan0@gmail.com>
2022-11-16 19:11:37 +0200

    api fetched

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ app.use(express.static("public"));
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+// app.get("/dinoname", async (req, res) => {
+//   res.send("hello");
+//   fetch("https://dinoipsum.com/api/?format=json&words=10&paragraphs=3")
+//     .then((response) => response.json())
+//     .then((data) => console.log(data))
+//     .catch((err) => console.error("Where did all the dinosaurs go?"));
+// });
--- public/hey.js
+++ public/hey.js
@@ -1,1 +1,5 @@
 console.log("script loadin");
+fetch("https://dinoipsum.com/api/?format=json&words=10&paragraphs=3")
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+  .catch((err) => console.error("Where did all the dinosaurs go?"));
